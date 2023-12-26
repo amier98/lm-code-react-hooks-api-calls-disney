79fe443feccf1d9c545a3857551df93cc0334156
Amier Ali <amier_1998@hotmail.com>
2023-12-26 01:19:51 +0000

    feat: Add fetch to get Star wars characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,35 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/header";
 import CharacterContainer from "./components/character_container";
 import Navigation from "./components/navigation";
 import { DisneyCharacter } from "./disney_character";
 
-
 const App: React.FC = () => {
-  
   const [currentPage, setCurrentPage] = useState<number>(1);
 
-    // Some dummy state representing disney characters
-    const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-      {
-        _id: 6,
-        name: "'Olu Mel",
-        imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-      },
-      {
-        _id: 25,
-        name: "Abu",
-        imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-      },
-      {
-        _id: 30,
-        name: "Ace",
-        imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-      },
-    ]);
+  // Some dummy state representing disney characters
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
+
+  useEffect(() => {
+    const getCharacters = async (pageNumber: number) => {
+      const result = await fetch(
+        `http://api.disneyapi.dev/character?page=${pageNumber}`
+      );
+      const res = (await result.json()) as { data: DisneyCharacter[] };
+      setCharacters(res.data);
+    };
+    getCharacters(2);
+  }, []);
+
+  console.log(characters);
 
   return (
-        <div className="page">
-          <Header currentPage={currentPage} />
-          <Navigation
-            currentPage={currentPage} setCurrentPage={setCurrentPage} />
-          <CharacterContainer characters={characters} />
-        </div>
+    <div className="page">
+      <Header currentPage={currentPage} />
+      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <CharacterContainer characters={characters} />
+    </div>
   );
 };
 
